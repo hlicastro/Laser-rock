60b142fe0656fc8fff234b9df48fdea54af2b17c
Hernan Licastro <bbremeras@gmail.com>
2022-01-10 20:17:19 -0300

    correcciones3

--- Script.js
+++ Script.js
@@ -1,25 +1,26 @@
 //Declaracion de variables
-let cantidadProducto = 0;
 let ingreso=0;
 let subtotal=0;
-const item1 =4560;
-const item2 =5000;
-const item3 =6000;
-const item4 =8000;
-const item5 =3000;
+let cantidadProducto=0;
 
 //Funciones
 const calcularIva =(importe) =>{return(importe*1.21)}
 const subtotales =(subtotal,producto) =>{return(subtotal+producto)}
 
+
 function mostrarTotal(cantidadProducto,subtotal)  { 
     
     alert(`Su pedido tiene esta compuesto de: ${cantidadProducto} items \nSubtotal es de: $ ${subtotal} \nTotal IVA incluido: $${calcularIva(subtotal)}`)  
 }
-
-function imprimir(descripcion,importe, cantidadProducto,subtotal) {
-    alert(`Agregaste ${descripcion} por un valor de $ ${importe} \nSu pedido tiene: ${cantidadProducto} items \nSubtotal es de:$ ${subtotal} \n Subtotal IVA incluido ${calcularIva(subtotal)} `)  
+function comprobarStock(stock,subtotal,cantidadProducto,subtotal,imprimir) {
+    
+    if(stock == 0){alert("Este producto se encuentra sin STOCK elija otro articulo")}
+        else{
+        imprimir.getImprimirArticulo(subtotal,cantidadProducto)
+        }
 }
+
+
 function descuento(cantidadProducto, subtotal) {
     if (cantidadProducto<3) {
         alert("No se aplica descuento")
@@ -27,55 +28,110 @@ function descuento(cantidadProducto, subtotal) {
         
     } else {
         alert("Por superar los 3 productos se aplica un 5% de descuento")
-        return subtotal*0.95
-        
+        subtotal*0.95
+        mostrarTotal(cantidadProducto,subtotal)
+
     }
     
 }
 
+class articulo{
+    constructor(item, artista, titulo, tipo, precio,stock) {
+        this.item = item;
+        this.artista   = artista;
+        this.titulo  = titulo;
+        this.tipo  = tipo;
+        this.precio  = precio;
+        this.stock  = stock;
+    }
+    getImprimirArticulo(subtotal,cantidadProducto){
+        alert("Agregaste un: " +this.tipo+ " -" +this.titulo +" de "+this.artista +" por un valor de $" +this.precio + "\nSu pedido tiene: " + cantidadProducto  +"\nSubtotal es de:$ "+subtotal+ "\nSubtotal IVA incluido $" +calcularIva(subtotal))  
+        this.stock =this.stock-1
+
+    }
+    
+
+}
+const articulo1 = new articulo(1, "La Renga", "Despedazados por mil partes","CD",1580, 2);
+const articulo2 = new articulo(2, "Divididos", "La era de la boludez","CD",1450, 4);
+const articulo3 = new articulo(3, "Las Pastillas del Abuelo", "2020","CD",1390,2);
+const articulo4 = new articulo(4, "The Rolling Stones", "On Air","CD",2120,5);
+const articulo5 = new articulo(5, "Los Piojos", "Ay, Ay, Ay","CD",1560,9);
+const articulo6 = new articulo(6, "Spinetta", "No mires Atas","CD",2110,3);
+const articulo7 = new articulo(7, "ACDC", "Back in Black","Vinilos",5850,1);
+const articulo8 = new articulo(8, "La Renga", "El Hojo Del Huracan","DVD",3175,2);
+
+
 //ingreso de productos
-while (ingreso !="ESC" ) {
-    ingreso = prompt(`Ingrese el numero del producto deseado: \n1_ Agregaste Prover Altaperformance 20kg por un valor de $ ${item1} \n2_ Pro Plan 15kg por un valor de $ ${item2} \n3_ Vital Can 20kg por un valor de $ ${item3} \n4_ Agregaste Total Balance 20kg por un valor de $ ${item4} \n5_ Raza perro adulto 21kg por un valor de $ ${item5} \nESC para finalizar la compra`)
+while (ingreso !=null) {
+    
+ingreso = prompt(`Ingrese el numero del producto deseado: o Precione Cancelar para Finalizar`)
 
 switch (ingreso) {
     case "1":
+        if(articulo1.stock == 0){alert("Este producto se encuentra sin STOCK elija otro articulo")}
+        else{
         cantidadProducto++
-        subtotal= subtotales(subtotal,item1)
-        imprimir("Prover Alta performance 20kg",item1,cantidadProducto,subtotal)
+        subtotal= subtotales(subtotal,articulo1.precio)
+        articulo1.getImprimirArticulo(subtotal,cantidadProducto)}
     break;
     case "2":
+        if(articulo2.stock == 0){alert("Este producto se encuentra sin STOCK elija otro articulo")}
+        else{
         cantidadProducto++
-        subtotal= subtotales(subtotal,item2)
-        imprimir("Pro Plan 15kg",item2,cantidadProducto,subtotal)
+        subtotal= subtotales(subtotal,articulo2.precio)
+        articulo2.getImprimirArticulo(subtotal,cantidadProducto)}
     break;
     case "3":
+        if(articulo3.stock == 0){alert("Este producto se encuentra sin STOCK elija otro articulo")}
+        else{
         cantidadProducto++
-        subtotal= subtotales(subtotal,item3)
-        imprimir("Vital Can 20kg",item3,cantidadProducto,subtotal)
+        subtotal= subtotales(subtotal,articulo3.precio)
+        articulo3.getImprimirArticulo(subtotal,cantidadProducto)}
     break;
     case "4":
+        if(articulo4.stock == 0){alert("Este producto se encuentra sin STOCK elija otro articulo")}
+        else{
         cantidadProducto++
-        subtotal= subtotales(subtotal,item4)
-        imprimir("Total Balance 20kg",item4,cantidadProducto,subtotal)
-
+        subtotal= subtotales(subtotal,articulo4.precio)
+        articulo4.getImprimirArticulo(subtotal,cantidadProducto)}
     break;
     case "5":
+        if(articulo5.stock == 0){alert("Este producto se encuentra sin STOCK elija otro articulo")}
+        else{
         cantidadProducto++
-        subtotal= subtotales(subtotal,item5)
-        imprimir("Raza perro adulto 21kg",item4,cantidadProducto,subtotal)
-
-    break;      
+        subtotal= subtotales(subtotal,articulo5.precio)
+        articulo5.getImprimirArticulo(subtotal,cantidadProducto)}
+    break;     
+    case "6":
+        if(articulo6.stock == 0){alert("Este producto se encuentra sin STOCK elija otro articulo")}
+        else{
+        cantidadProducto++
+        subtotal= subtotales(subtotal,articulo6.precio)
+        articulo6.getImprimirArticulo(subtotal,cantidadProducto)}
+    break;   
+    case "7":
+        if(articulo7.stock == 0){alert("Este producto se encuentra sin STOCK elija otro articulo")}
+        else{
+        cantidadProducto++
+        subtotal= subtotales(subtotal,articulo7.precio)
+        articulo7.getImprimirArticulo(subtotal,cantidadProducto)}
+    break;   
+    case "8":
+        if(articulo8.stock == 0){alert("Este producto se encuentra sin STOCK elija otro articulo")}
+        else{
+        cantidadProducto++
+        subtotal= subtotales(subtotal,articulo8.precio)
+        articulo8.getImprimirArticulo(subtotal,cantidadProducto)}
+    break;    
     default:
-        if (ingreso !="ESC") {alert("No ingreso un item valido, elija un nuevo item ") }
+        if (ingreso !=null) {alert("No ingreso un item valido, elija un nuevo item ") }
 
         break;
     
     }
     
 }
+
 mostrarTotal(cantidadProducto,subtotal)
 subtotal=descuento(cantidadProducto, subtotal)
-mostrarTotal(cantidadProducto,subtotal)
-
-
-
